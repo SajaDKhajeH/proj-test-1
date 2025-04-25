247413c693ddd8b1a33dc8dce17e61549219d95f
Sajad khajeh <sajad@DESKTOP-0J20JC9>
2025-04-25 16:54:22 +0330

    init project

---
+++ Form1.cs
@@ -0,0 +1,11 @@
+using System.Windows.Forms;
+
+namespace proj_test_1;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Windows.Forms;
+
+namespace proj_test_1;
+
+static class Program
+{
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main(string[] args)
+    {
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        // LastChangeOfThisCommit
+
+        ApplicationConfiguration.Initialize();
+        Application.Run(new Form1());
+    }
+}
