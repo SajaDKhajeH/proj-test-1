a45fe94704452f7bceb55f0b50ca77dfd7a34215
Sajad khajeh <sajad@DESKTOP-0J20JC9>
2025-04-26 15:37:24 +0330

    //bug fixx in bugfixx

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ static class Program
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
         // LastChangeOfThisCommit
-
+        //bug fixx
         ApplicationConfiguration.Initialize();
         Application.Run(new Form1());
     }
